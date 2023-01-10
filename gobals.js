417b81efe20eeabd875da369eb3d3053c9e500d3
aratoca <afbayonac@gmail.com>
2023-01-10 11:53:05 -0500

    Add globals

---
+++ gobals.js
@@ -0,0 +1,15 @@
+const api = 'http://192.168.20.109:3100'
+const firebaseConfig = {
+  apiKey: 'AIzaSyD60p-7UCjiX1OqMehvrFGHaD3rBT3gqLk',
+  authDomain: 'playground-9303c.firebaseapp.com',
+  projectId: 'playground-9303c',
+  storageBucket: 'playground-9303c.appspot.com',
+  messagingSenderId: '451250789927',
+  appId: '1:451250789927:web:01742390d5d1bf4aaf7802',
+  measurementId: 'G-E3W50S83ST'
+}
+
+export {
+  api,
+  firebaseConfig
+}
